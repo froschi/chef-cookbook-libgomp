55b337465ba61b7da9a2e180df07c1afd487f81b
Thorsten Fischer <thorsten@froschi.org>
2012-06-07 20:05:03 +0200

    Amended to be usable on lucid and precise.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,11 @@
-packages = %w/
-  libgomp1
-/
+packages = Array.new
+
+case node[:lsb][:codename]
+when "lucid", "precise"
+  packages = %w/
+    libgomp1
+  /
+end
 
 packages.each do |pkg|
   package pkg do
